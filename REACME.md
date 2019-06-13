89b5784b35f0e8715d6489db1b996341ac7866a5
BrianWang <wangyu121x@gmail.com>
2019-06-14 04:16:39 +0800

    to config grpc

---
+++ REACME.md
@@ -0,0 +1,12 @@
+# Java Tensorflow Serving Grpc Client Demo
+
+
+
+## Download Demo Resnet Model
+
+
+`mkdir models`
+
+`mkdir models/resnet`
+
+`curl -s https://storage.googleapis.com/download.tensorflow.org/models/official/20181001_resnet/savedmodels/resnet_v2_fp32_savedmodel_NHWC_jpg.tar.gz | tar --strip-components=2 -C ./models/resnet -xvz`
---
+++ src/main/java/brian/wang/tf/serving/DemoApplication.java
@@ -0,0 +1,54 @@
+package brian.wang.tf.serving;
+
+import com.google.protobuf.ByteString;
+import io.grpc.ManagedChannel;
+import io.grpc.ManagedChannelBuilder;
+import java.io.File;
+import org.apache.commons.io.FileUtils;
+import java.io.IOException;
+
+import org.tensorflow.framework.DataType;
+import org.tensorflow.framework.TensorProto;
+import org.tensorflow.framework.TensorShapeProto;
+
+import tensorflow.serving.Model;
+import tensorflow.serving.Predict;
+import tensorflow.serving.PredictionServiceGrpc;
+
+
+
+public class DemoApplication {
+
+    public static void main(String[] args){
+        byte[] fileContent = new byte[0];
+        try {
+            fileContent = FileUtils.readFileToByteArray(new File("./src/main/resources/images/car.jpg"));
+            String host = "127.0.0.1";
+            int port = 8500;
+            ManagedChannel channel = ManagedChannelBuilder.forAddress(host, port).usePlaintext(true).maxInboundMessageSize(1073741824).build();
+            PredictionServiceGrpc.PredictionServiceBlockingStub stub = PredictionServiceGrpc.newBlockingStub(channel);
+
+            Predict.PredictRequest.Builder predictRequestBuilder = Predict.PredictRequest.newBuilder();
+            Model.ModelSpec.Builder modelSpec = Model.ModelSpec.newBuilder();
+            modelSpec.setName("resnet");
+            predictRequestBuilder.setModelSpec(modelSpec);
+
+
+            TensorProto.Builder tensorProto = TensorProto.newBuilder();
+            TensorShapeProto.Builder tensorShapeBuilder = TensorShapeProto.newBuilder().addDim(TensorShapeProto.Dim.newBuilder().setSize(1));
+
+            tensorProto.setDtype(DataType.DT_STRING);
+            tensorProto.setTensorShape(tensorShapeBuilder.build());
+            tensorProto.addStringVal(ByteString.copyFrom(fileContent));
+
+            predictRequestBuilder.putInputs("image_bytes", tensorProto.build());
+            Predict.PredictResponse predictResponse = stub.predict(predictRequestBuilder.build());
+
+            System.out.println("classes are: " + predictResponse.getOutputsOrThrow("classes").getInt64Val(0));
+            System.out.println("probabilities are: " + predictResponse.getOutputsOrThrow("probabilities").getFloatVal(0));
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+
+    }
+}
